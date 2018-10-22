7b955a0997c51e7b859062314b39e7bfa9720590
slotwa <ssijajajamurzynowi@gmail.com>
2018-10-22 20:23:02 +0200

    init changes

--- assets/js/now-ui-kit.js
+++ assets/js/now-ui-kit.js
@@ -255,7 +255,7 @@ nowuiKit = {
     }, 6),
 
     initContactUsMap: function() {
-        var myLatlng = new google.maps.LatLng(40.748817, -73.985428);
+        var myLatlng = new google.maps.LatLng(52.223535, 21.019249);
         var mapOptions = {
             zoom: 13,
             center: myLatlng,
